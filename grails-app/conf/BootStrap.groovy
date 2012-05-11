c228bc1a420c21a70cac0f96e34299f0127d70b3
Blessilda <blesssilda@lenovo1.manila.orangeandbronze.com>
2012-05-11 09:50:23 +0800

    bootstrap with SnaphUsers and Categories
    Signed-off-by: Blessilda <blesssilda@lenovo1.manila.orangeandbronze.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,36 @@
+import com.onb.snaph.*
+
 class BootStrap {
 
     def init = { servletContext ->
+	def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+      def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+
+      def snaphUser1 = new SnaphUser(username: 'blessie', enabled: true, password: 'blessie', facebookID: '100000081560972')
+      snaphUser1.save(flush: true)
+
+      def snaphUser2 = new SnaphUser(username: 'hazel', enabled: true, password: 'hazel', facebookID: '566924412')
+      snaphUser2.save(flush: true)
+
+      def snaphUser3 = new SnaphUser(username: 'giselle', enabled: true, password: 'giselle', facebookID: '521537572')
+      snaphUser3.save(flush: true)
+      
+      UserRole.create snaphUser1, adminRole, true
+	UserRole.create snaphUser2, userRole, true
+	UserRole.create snaphUser3, userRole, true
+
+      assert User.count() == 3
+      assert Role.count() == 2
+      assert UserRole.count() == 3
+
+	def electronics = new Category(category: 'Electronics').save(flush: true)
+	def fashion = new Category(category: 'Fashion').save(flush: true)
+	def toys = new Category(category: 'Toys').save(flush: true)
+	def laptops = new Category(category: 'Laptops').save(flush: true)
+	def console = new Category(category: 'Console').save(flush: true)
+	
+	assert Category.count == 5
+
     }
     def destroy = {
     }
