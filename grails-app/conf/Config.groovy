d606971b3eb4e6f9758b43656acb7bfa7d4175cb
hzel <hazel.tan@orangeandbronze.com>
2012-05-11 18:32:33 +0800

    auto generated after s2-quickstart for spring-security-facebook

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -99,3 +99,8 @@ log4j = {
 grails.plugins.springsecurity.userLookup.userDomainClassName = 'com.onb.snaph.User'
 grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'com.onb.snaph.UserRole'
 grails.plugins.springsecurity.authority.className = 'com.onb.snaph.Role'
+
+
+grails.plugins.springsecurity.facebook.domain.classname='com.onb.snaph.FacebookUser'
+grails.plugins.springsecurity.facebook.appId='127828560685665'
+grails.plugins.springsecurity.facebook.secret='7c9b96306e4a35af94a32bf320da20aa'
