51008a61e1ca9454b1868ddda31d45c0592af59e
Blessilda <blesssilda@lenovo1.manila.orangeandbronze.com>
2012-05-15 17:05:39 +0800

    updated BootStrap
    Signed-off-by: Blessilda <blesssilda@lenovo1.manila.orangeandbronze.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -14,22 +14,26 @@ class BootStrap {
 
       def snaphUser3 = new SnaphUser(username: 'giselle', enabled: true, password: 'giselle', facebookID: '521537572')
       snaphUser3.save(flush: true)
+	  
+	  def snaphUser4 = new SnaphUser(username: 'dummy', enabled: true, password: 'dummy', facebookID: '100003804984482')
+	  snaphUser4.save(flush: true)
       
       UserRole.create snaphUser1, adminRole, true
 	UserRole.create snaphUser2, userRole, true
 	UserRole.create snaphUser3, userRole, true
+	UserRole.create snaphUser4, userRole, true
 
-      assert User.count() == 3
+      assert User.count() == 4
       assert Role.count() == 2
-      assert UserRole.count() == 3
+      assert UserRole.count() == 4
 
-	def electronics = new Category(category: 'Electronics').save(flush: true)
-	def fashion = new Category(category: 'Fashion').save(flush: true)
-	def toys = new Category(category: 'Toys').save(flush: true)
-	def laptops = new Category(category: 'Laptops').save(flush: true)
-	def console = new Category(category: 'Console').save(flush: true)
-	
-	assert Category.count == 5
+//	def electronics = new Category(category: 'Electronics').save(flush: true)
+//	def fashion = new Category(category: 'Fashion').save(flush: true)
+//	def toys = new Category(category: 'Toys').save(flush: true)
+//	def laptops = new Category(category: 'Laptops').save(flush: true)
+//	def console = new Category(category: 'Console').save(flush: true)
+//	
+//	assert Category.count == 5
 
     }
     def destroy = {
