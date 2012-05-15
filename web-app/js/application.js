c2a23b3e9c684492fe5799307c45a1c4838c7302
giselleling <giselleling@yahoo.com>
2012-05-15 17:17:29 +0800

    updated application.js with popup function (jquery).
    Signed-off-by: giselleling <giselleling@yahoo.com>

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -7,3 +7,81 @@ if (typeof jQuery !== 'undefined') {
 		});
 	})(jQuery);
 }
+
+			function showDetailedItem(itemInstance){
+				$('.detailed_item_name').html('This existing content has been replaced with this content.');
+				document.getElementById('container').style.visibility="visible";
+			}
+			function hideDetailedItem(){
+				document.getElementById('container').style.visibility="hidden";
+			}
+			function showUserGallery(){
+				document.getElementById('user_gallery').style.visibility="visible";
+			}
+			function hideUserGallery(){
+				document.getElementById('user_gallery').style.visibility="hidden";
+			}
+
+
+﻿/***************************/
+//@Author: Adrian "yEnS" Mato Gondelle
+//@website: www.yensdesign.com
+//@email: yensamg@gmail.com
+//@license: Feel free to use it, but keep this credits please!					
+/***************************/
+
+//SETTING UP OUR POPUP
+//0 means disabled; 1 means enabled;
+var popupStatus = 0;
+
+//loading popup with jQuery magic!
+function loadPopup(){
+	//loads popup only if it is disabled
+	if(popupStatus==0){
+		$(".details_outer_div").css({
+			"opacity": "0.7"
+		});
+		$(".details_outer_div").fadeIn("slow");
+		$(".details_div").fadeIn("slow");
+		popupStatus = 1;
+	}
+}
+
+//disabling popup with jQuery magic!
+function disablePopup(){
+	//disables popup only if it is enabled
+	if(popupStatus==1){
+		$(".details_outer_div").fadeOut("slow");
+		$(".details_div").fadeOut("slow");
+		popupStatus = 0;
+	}
+}
+
+
+//CONTROLLING EVENTS IN jQuery
+$(document).ready(function(){
+	
+	//LOADING POPUP
+	//Click the button event!
+	$(".details_button").click(function(){
+		//load popup
+		loadPopup();
+	});
+				
+	//CLOSING POPUP
+	//Click the x event!
+	$(".details_divClose").click(function(){
+		disablePopup();
+	});
+	//Click out event!
+	$(".details_outer_div").click(function(){
+		disablePopup();
+	});
+	//Press Escape event!
+	$(document).keypress(function(e){
+		if(e.keyCode==27 && popupStatus==1){
+			disablePopup();
+		}
+	});
+
+});
