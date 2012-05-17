a5d8b76d89f763dc62558ab2f38b3c4198ee5558
giselleling <giselleling@yahoo.com>
2012-05-17 11:59:25 +0800

    Modified UploadController.groovy to enable insert, edit, delete commands from Android app.
    Signed-off-by: giselleling <giselleling@yahoo.com>

--- grails-app/controllers/com/onb/snaph/UploadController.groovy
+++ grails-app/controllers/com/onb/snaph/UploadController.groovy
@@ -1,8 +1,5 @@
 package com.onb.snaph
 
-
-//import org.apache.hadoop.hbase.util.Bytes
-
 import grails.converters.XML
 import java.text.DateFormat
 import java.text.SimpleDateFormat
@@ -15,56 +12,22 @@ class UploadController {
 
 	def uploadData(){
 		if(request.method == "POST"){
-			System.out.println("posted");
-			//			response.status = 200
-			//			render "posted"
-			//		}
 			if(request instanceof MultipartHttpServletRequest){
 				MultipartHttpServletRequest mpr = (MultipartHttpServletRequest)request;
-
-				String token = params.token
-				String name = params.name
-				//				def image1 = params.image
-				//				System.out.println("your image type is:"+image1)
-				def imageFile = params.image
-				byte[] image = imageFile.getBytes()
-//				System.out.println("image string:"+new String(image));
-//				System.out.println("image uri:"+new URI( Bytes.toString(image)));
-				String desc = params.description
-				String price = params.price
-
-				//				store data to database
-				Item newItem = new Item()
-				newItem.itemName  = name
-				newItem.image = image
-				newItem.description = desc
-				newItem.price = price
-//				newItem.category = Category.findByCategory("Electronics")
-				SnaphUser user = SnaphUser.findByFacebookID(token)
-				//if user is not in database, make a new user
-				if(user == null){
-					user = new SnaphUser()
-					user.facebookID = token
-					user.username = "username"
-					user.password = "password"
-					user.enabled = true 
-					user.save(flush: true)
-					}
-				newItem.snaphUser = user
-				newItem.comments = null
-
-				DateFormat dateFormat = new SimpleDateFormat("MMMMM dd yyyy")
-				Date date = new Date()
-				newItem.dateAdded = dateFormat.parse(dateFormat.format(date))
-
-				if(newItem.save(flush: true)){
-					response.status = 200 // Created
-					System.out.println "name:"+ name + " image:"+ imageFile + " desc:"+ desc + " price:"+ price +" token:"+token+" dateAdded:"+newItem.dateAdded+"\n"
-					render "saved item uploaded with name:"+ name+" image:"+ imageFile+" desc:"+ desc+" price:"+ price + " token:"+token+" dateAdded:"+newItem.dateAdded+"\n"
-				}
-				else{
-					response.status = 500 //Internal Server Error
-					render "Could not create new item due to errors:\n ${newItem.errors}"
+				switch(params.command){
+					case "insert":
+						insertItem(params)
+						break
+					case "delete":
+						deleteItem(params)
+						break
+					case "edit":
+						editItem(params)
+						break
+					default:
+						response.status = 404 //Internal Server Error
+						render "Invalid command."
+						break
 				}
 			}
 			else{
@@ -74,6 +37,105 @@ class UploadController {
 		}
 		else{
 			System.out.println("invalid request. can only accept post");
+		}
+	}
+
+	def insertItem = { params ->
+		String token = params.token
+		String userId = params.userId
+		String name = params.name
+		def imageFile = params.image
+		byte[] image = imageFile.getBytes()
+		String desc = params.description
+		String price = params.price
+
+		Item newItem = new Item()
+		newItem.itemName  = name
+		newItem.image = image
+		newItem.description = desc
+		newItem.price = price
+		//				newItem.category = Category.findByCategory("Electronics")
+		SnaphUser user = SnaphUser.findByFacebookID(userId)
+
+		//				if user is not in database, make a new user
+		if(user == null){
+			user = new SnaphUser()
+			user.facebookID = userId
+			user.username = "username"
+			user.password = "password"
+			user.enabled = true
+			user.save(flush: true)
+		}
+		newItem.snaphUser = user
+		newItem.comments = null
+
+		DateFormat dateFormat = new SimpleDateFormat("MMMMM dd yyyy")
+		Date date = new Date()
+		newItem.dateAdded = dateFormat.parse(dateFormat.format(date))
+
+		if(newItem.save(flush: true)){
+			response.status = 201 // Created
+			System.out.println "name:"+ name + " image:"+ imageFile + " desc:"+ desc + " price:"+ price +" userId:"+userId+" dateAdded:"+newItem.dateAdded+"\n"
+			render "saved item uploaded with name:"+ name+" image:"+ imageFile+" desc:"+ desc+" price:"+ price + " userId:"+userId+" dateAdded:"+newItem.dateAdded+"\n"
+		}
+		else{
+			response.status = 500 //Internal Server Error
+			render "Could not create new item due to errors:\n ${newItem.errors}"
+		}
+	}
+
+	def editItem = { params ->
+		def itemId = params.itemId
+		def userId = params.userId
+		def itemInstance = Item.findById(itemId)
+		if(itemInstance == null){
+			response.status = 404	//Unauthorized
+			render "Could not edit item "+itemId+" because it does not exist."
+			return
+		}
+		def itemOwner = itemInstance.snaphUser
+		if(itemOwner.facebookID == userId){
+			itemInstance.itemName = params.name
+			itemInstance.price = params.price
+			itemInstance.description = params.description
+			def imageFile = params.image
+			byte[] image = imageFile.getBytes()
+			itemInstance.image = image
+			if(itemInstance.save(flush: true)){
+				response.status = 200 // OK
+				System.out.println "name:"+ itemInstance.itemName + " image:"+ imageFile + " desc:"+ itemInstance.description + " price:"+ itemInstance.price +" userId:"+userId+" dateAdded:"+itemInstance.dateAdded+"\n"
+				render "edited item "+itemInstance.id+" name:"+itemInstance.itemName + " image:"+ imageFile + " desc:"+ itemInstance.description + " price:"+ itemInstance.price +" userId:"+userId+" dateAdded:"+itemInstance.dateAdded+"\n"
+			}
+			else{
+				response.status = 500 //Internal Server Error
+				render "Could not create new item due to errors:\n ${itemInstance.errors}"
+			}
+		}
+		else{
+			response.status = 401	//Unauthorized
+			render "Could not edit item " + itemId + ". Proper authority needed."
+		}
+	}
+
+	def deleteItem = { params ->
+		def itemId = params.itemId
+		def userId = params.userId
+		def itemInstance = Item.findById(itemId)
+		if(itemInstance == null){
+			response.status = 404	//Unauthorized
+			render "Could not delete item "+ itemId +" because it does not exist."
+			return
+		}
+		def itemOwner = itemInstance.snaphUser
+		if(itemOwner.facebookID == userId){
+			itemInstance.delete()
+			response.status = 200 // OK
+			System.out.println "item " + itemId + " was deleted\n"
+			render "item "+ itemId + " was deleted\n"
+		}
+		else{
+			response.status = 401	//Unauthorized
+			render "Could not delete item "+itemId+". Proper authority needed."
 		}
 	}
 }
