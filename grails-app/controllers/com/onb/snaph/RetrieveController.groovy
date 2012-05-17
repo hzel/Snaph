6c1503e7076abb892f474a3a05c8ab2452e556ca
giselleling <giselleling@yahoo.com>
2012-05-17 11:44:04 +0800

    Added itemURL to RetrieveController.groovy
    Signed-off-by: giselleling <giselleling@yahoo.com>

--- grails-app/controllers/com/onb/snaph/RetrieveController.groovy
+++ grails-app/controllers/com/onb/snaph/RetrieveController.groovy
@@ -1,14 +1,16 @@
 package com.onb.snaph
 
+import java.net.*
 import grails.converters.JSON
 
 class RetrieveController {
 
-	String ipAddress = "10.10.5.122"
+	//	String ipAddress = java.net.InetAddress.getLocalHost().getHostAddress()
+	String ipAddress = "10.10.5.51"
 	String port = "8080"
-	
-    def index() { }
-	
+
+	def index() { }
+
 	def retrieveData(){
 		if(request.method == "POST"){
 			HashMap jsonMap = new HashMap()
@@ -18,26 +20,25 @@ class RetrieveController {
 			if(user == null){
 				System.out.println("user not found!");
 			}
-			else
-				System.out.println("user id:"+user.id);
+			System.out.println("retrieving data for userId:"+user.id);
 			List<Item> itemList = user.items.toList()
 			System.out.println("list size:"+itemList.size());
 			jsonMap.items = itemList.collect {item ->
 				byte[] image = item.image
-				System.out.println("item encoded");
 				String uri = "http://"+ipAddress+":"+port+"/Snaph/image/"+item.id
-				return [name: item.itemName, price: item.price, description: item.description, image:uri]
+				String itemURL = "http://"+ipAddress+":"+port+"/Snaph/item_profile/"+item.id
+				return [name: item.itemName, price: item.price, description: item.description, image:uri, itemId:item.id, itemUrl:itemURL ]
 			}
-			
-			System.out.println("finished making jsonMap:"+jsonMap.toString());
+
 			response.status = 200
 			render(contentType: "text/json") {
 				jsonMap
 			}
+			System.out.println("finished making jsonMap:"+jsonMap.toString());
 			
-			}
+		}
 		else{
 			System.out.println("invalid request. can only accept post");
-			}
 		}
+	}
 }
