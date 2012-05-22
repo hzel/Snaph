e8083a1e63ac280cdc63b8f196d85d7a461e8df6
hzel <hazel.tan@orangeandbronze.com>
2012-05-22 16:51:11 +0800

    removed unused imports
    Signed-off-by: hzel <hazel.tan@orangeandbronze.com>

--- grails-app/domain/com/onb/snaph/FacebookUser.groovy
+++ grails-app/domain/com/onb/snaph/FacebookUser.groovy
@@ -2,9 +2,6 @@ package com.onb.snaph
 
 import com.onb.snaph.User
 import com.the6hours.grails.springsecurity.facebook.FacebookUserDomain;
-import org.springframework.social.facebook.api.Facebook
-import org.springframework.social.facebook.api.FacebookProfile
-import org.springframework.social.facebook.api.impl.FacebookTemplate
 
 class FacebookUser implements FacebookUserDomain{
 
