b13f54510c08129166f3825689baada5c94be425
giselleling <giselleling@yahoo.com>
2012-05-22 16:16:23 +0800

    modified UrlMappings.groovy - added mapping for create item.
    Signed-off-by: giselleling <giselleling@yahoo.com>

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,6 +6,7 @@ class UrlMappings {
 				// apply constraints here
 			}
 		}
+		"/item/create"(view: "/item/create")
 		"/upload"(controller:"Upload",action:"uploadData")
 		"/retrieve/$token?"(controller:"Retrieve",action:"retrieveData")
 		"/image/$id?"(controller:"Item",action:"displayLogo")
