d50b74514af391c108e3d04dd5c63d68e34a1699
hzel <hazel.tan@orangeandbronze.com>
2012-05-22 16:29:55 +0800

    Test for FacebookProfileDetailService
    Signed-off-by: hzel <hazel.tan@orangeandbronze.com>

---
+++ test/unit/com/onb/snaph/FacebookProfileDetailServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.onb.snaph
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(FacebookProfileDetailService)
+class FacebookProfileDetailServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
