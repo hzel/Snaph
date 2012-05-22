774e67dbceadc6d5bff645e22f7ad179dfded51a
hzel <hazel.tan@orangeandbronze.com>
2012-05-22 16:30:53 +0800

    Tests for facebook template
    Signed-off-by: hzel <hazel.tan@orangeandbronze.com>

---
+++ test/unit/com/the6hours/grails/springsecurity/facebook/FacebookTemplateTests.groovy
@@ -0,0 +1,17 @@
+package com.the6hours.grails.springsecurity.facebook
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(FacebookTemplate)
+class FacebookTemplateTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
