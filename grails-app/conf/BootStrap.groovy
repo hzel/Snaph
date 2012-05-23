2d202743f9d8da4fa3b0d4003cb91483f600708f
hzel <hazel.tan@orangeandbronze.com>
2012-05-23 11:50:23 +0800

    updated bootstrap
    Signed-off-by: hzel <hazel.tan@orangeandbronze.com>

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,16 +9,24 @@ class BootStrap {
 
 		def snaphUser1 = new SnaphUser(username: 'blessie', enabled: true, password: 'blessie', facebookID: '100000081560972')
 		snaphUser1.save(flush: true)
+		def facebookUser1 = new FacebookUser(uid: Long.parseLong('100000081560972'), accessToken: 'token')
+		facebookUser1.save(flush:true)
 
 		def snaphUser2 = new SnaphUser(username: 'hazel', enabled: true, password: 'hazel', facebookID: '566924412')
 		snaphUser2.save(flush: true)
-
+		def facebookUser2 = new FacebookUser(uid: Long.parseLong('566924412'), accessToken: 'token')
+		facebookUser2.save(flush:true)
+		
 		def snaphUser3 = new SnaphUser(username: 'giselle', enabled: true, password: 'giselle', facebookID: '521537572')
 		snaphUser3.save(flush: true)
-
+		def facebookUser3 = new FacebookUser(uid: Long.parseLong('521537572'), accessToken: 'token')
+		facebookUser3.save(flush:true)
+		
 		def snaphUser4 = new SnaphUser(username: 'dummy', enabled: true, password: 'dummy', facebookID: '100003804984482')
 		snaphUser4.save(flush: true)
-
+		def facebookUser4 = new FacebookUser(uid: Long.parseLong('100003804984482'), accessToken: 'token')
+		facebookUser4.save(flush:true)
+		
 		UserRole.create snaphUser1, adminRole, true
 		UserRole.create snaphUser2, userRole, true
 		UserRole.create snaphUser3, userRole, true
